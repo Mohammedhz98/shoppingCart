3e0a757e533fe6c2ac22bf46f7bf7e41edaa255b
Mohammedhz98 <mohammedhz98@hotmail.com>
2021-08-19 17:48:34 +0100

    The shopping cart can only calculate the total cost and remove and add products of different quantities.

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -13,13 +13,46 @@ public class ShoppingCart {
         System.out.println("How many '" + product.getName() + "'s' would you like to add to your basket? ");
         Scanner howMany = new Scanner(System.in);
         int quantity = howMany.nextInt();
+        while(quantity > product.getQuantityInStock()) {
+                System.out.println("I'm sorry we cannot add " + quantity + " '" + product.getName() + "' to your basket as" +
+                        " we only have " + product.getQuantityInStock() + " '" + product.getName() + "'s' in stock" +
+                        " please input a quantity less than the amount in stock.");
+                howMany = new Scanner(System.in);
+                quantity = howMany.nextInt();
+        }
         shoppingBasket.add(new ItemInCart(product.getName(),product.getPrice(),product.getSale(),product.getQuantityInStock(),quantity));
+        System.out.println(quantity + " '" + product.getName() + "'s' added to the basket.");
     }
 
     public void removeItemFromCart(Item product) {
+        int itemQuantity = 0;
         for(ItemInCart i: shoppingBasket) {
             if(i.getName().equals(product.getName())) {
+                itemQuantity = i.getQuantity();
+            }
+        }
+        System.out.println("How many '" + product.getName() + "' would you like to remove form the basket? ");
+        Scanner howMany = new Scanner(System.in);
+        int remove = howMany.nextInt();
+        while(remove > itemQuantity) {
+            System.out.println("You cannot remove " + remove + " '" + product.getName() + "'s' from your basket" +
+                               " due to there being only " + itemQuantity + " '" + product.getName() + "'s' in your" +
+                               " basket. Please input an amount either less than or the same as the amount in your" +
+                               " basket." );
+            howMany = new Scanner(System.in);
+            remove = howMany.nextInt();
+        }
+        for(ItemInCart i: shoppingBasket) {
+            if(i.getName().equals(product.getName()) && itemQuantity == remove) {
                 shoppingBasket.remove(i);
+                System.out.println(i.getName() + " has been removed from the basket.");
+            }
+            else if(i.getName().equals(product.getName())){
+                int itemsLeft = itemQuantity - remove;
+                i.setQuantity(itemsLeft);
+                System.out.println(remove + " '" + product.getName() + "'s' have been removed from your basket. There" +
+                                   " are now " + i.getQuantity() + " '" + " '" + i.getName() + "'s' left in your " +
+                                   " basket." );
             }
         }
     }
@@ -37,20 +70,21 @@ public class ShoppingCart {
     }
 
     public String toString() {
-        String itemList = "";
-        for(Item i: shoppingBasket) {
+        StringBuilder itemList = new StringBuilder();
+        for(ItemInCart i: shoppingBasket) {
             if(i == shoppingBasket.get(shoppingBasket.size() -1)) {
-                itemList += " " + i.getName() + ".";
+                itemList.append(" "+ i.getQuantity() + " " + i.getName() + "'s.");
             }
             else {
-                itemList += " " + i.getName() + ",";
+                itemList.append(" " + i.getQuantity() + " " + i.getName() + "'s,");
             }
         }
         if(shoppingBasket.isEmpty()) {
             return "Your shopping basket is currently empty.";
         }
         else
-          return "Your shopping basket currently has the following items:" + itemList;
+          return "Your shopping basket currently has the following items:" + itemList + " Which comes to a total price" +
+                  " of \u00A3" + ShoppingCart.this.totalCartPrice();
     }
 
     public static void main(String[] args) {
@@ -67,7 +101,7 @@ public class ShoppingCart {
         System.out.println(myStuff);
         myStuff.removeItemFromCart(steak);
         System.out.println(myStuff);
-        System.out.println(myStuff.totalCartPrice());
-        System.out.println(String.valueOf(myBasket));
+        //System.out.println(myStuff.totalCartPrice());
+        //System.out.println(String.valueOf(myBasket));
     }
 }
